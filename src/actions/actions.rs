d5dc50d8513ed7fa03e0bd28659cb7fb32253775
BenCassidy33 <ben.d.cassidy@gmail.com>
2023-07-17 15:49:27 -0400

    updated names and added ability to get muiltiple tasks by a single project id

--- src/actions/actions.rs
+++ src/actions/actions.rs
@@ -29,6 +29,30 @@ pub fn get_project_by_id(id: String) -> Result<Project, ErrorType> {
     Err(ErrorType::User(UsrErrorType::InvalidProject))
 }
 
-pub fn read_task() -> Task {
-    todo!()
+pub fn get_task_by_id(id: String) -> Result<Task, ErrorType> {
+    let contents: Result<String, std::io::Error> = std::fs::read_to_string("src/data.json");
+    let data: Vec<Project> = serde_json::from_str(contents.unwrap().as_str()).unwrap();
+
+    for project in data.iter() {
+        for project_task in project.tasks.clone().into_iter() {
+            if project_task.id == id {
+                return Ok(project_task);
+            }
+        }
+    }
+
+    Err(ErrorType::User(UsrErrorType::InvalidTask))
+}
+
+pub fn get_tasks(project_id: String) -> Result<Vec<Task>, ErrorType> {
+    let contents: Result<String, std::io::Error> = std::fs::read_to_string("src/data.json");
+    let data: Vec<Project> = serde_json::from_str(contents.unwrap().as_str()).unwrap();
+
+    for project in data.iter() {
+        if project.id == project_id {
+            return Ok(project.tasks.clone());
+        }
+    }
+
+    Err(ErrorType::User(UsrErrorType::InvalidTask))
 }
--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,43 @@ pub mod types;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_project))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(get_project_by_id)
+            .service(get_task_by_id)
+            .service(get_tasks_by_project_id)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
 
-#[get("/api/v1/project/{project_id}")]
-async fn get_project(project_id: web::Path<String>) -> impl Responder {
+#[get("/api/v1/project/project/{project_id}")]
+async fn get_project_by_id(project_id: web::Path<String>) -> impl Responder {
     let project = actions::actions::get_project_by_id(String::from(project_id.into_inner()));
 
     return match project {
         Ok(project) => HttpResponse::Ok().json(project),
+        Err(_) => HttpResponse::Ok().json(json!({"error": "Project Not Found", "code": 404})),
+    };
+}
+
+#[get("/api/v1/project/task/{task_id}")]
+async fn get_task_by_id(task_id: web::Path<String>) -> impl Responder {
+    let task = actions::actions::get_task_by_id(String::from(task_id.into_inner()));
+
+    return match task {
+        Ok(project) => HttpResponse::Ok().json(project),
+        Err(_) => HttpResponse::Ok().json(json!({"error": "Project Not Found", "code": 404})),
+    };
+}
+
+#[get("/api/v1/project/tasks/{project_id}")]
+async fn get_tasks_by_project_id(project_id: web::Path<String>) -> impl Responder {
+    let tasks = actions::actions::get_tasks(String::from(project_id.into_inner()));
+
+    return match tasks {
+        Ok(tasks) => HttpResponse::Ok().json(tasks),
         Err(_) => HttpResponse::Ok().json(json!({"error": "Project Not Found", "code": 404})),
     };
 }
